a0cdde11b3e2f49ac129e414b9009c393e888af0
LebbeusAWoods <lawvim7@gmail.com>
2026-04-23 01:59:01 +0000

    Fixed git ignore to fully ignore log. Added tick rate to game, forcing run at 60 fps.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     #Start game loop
     while True:
@@ -18,7 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
